0d3f9df74c8e48975fbfde69de5c1458626c17f8
edsroque10 <66292608+edsroque10@users.noreply.github.com>
2020-07-10 17:05:52 +0800

    EL CHUPA KABRA

---
+++ styles/js/script.js
@@ -0,0 +1,13 @@
+  window.onscroll = function() {myFunction()};
+  var x = document.getElementById("test");
+
+  function myFunction(){
+if (window.pageYOffset >= x.offsetTop - 150) {
+  chuchu.classList.add("x");
+  fills.classList.add("fills");
+} else {
+  chuchu.classList.remove("x");
+  fills.classList.remove("fills");
+
+}
+}
